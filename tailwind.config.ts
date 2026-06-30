99b1148119c8a7bcc07ab9a93b67d6614c34f56f
DYP-website <arvin.bokhoree@live.it>
2026-06-30 03:53:21 +0200

    Delete tailwind.config.ts

--- tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from 'tailwindcss';
-const config: Config = {
-  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
-  theme: { extend: { boxShadow: { soft: '0 24px 80px rgba(82,64,130,.13)' } } },
-  plugins: []
-};
-export default config;
